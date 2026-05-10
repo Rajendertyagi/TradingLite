fdd1a7cf0a59992a531331f0a80a574fabf12406
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-10 13:52:42 +0530

    Update App.xaml.cs

--- TradingBrowser/App.xaml.cs
+++ TradingBrowser/App.xaml.cs
@@ -1,20 +1,54 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 
 namespace TradingBrowser
 {
     public partial class App : Application
     {
+        private Mutex? _mutex;
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int SW_RESTORE = 9;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            // CATCH ALL UNHANDLED UI THREAD CRASHES
+            _mutex = new Mutex(true, "Global\\TradingBrowser_SingleInstance_Mutex_987654321", out bool isNewInstance);
+            
+            if (!isNewInstance)
+            {
+                var currentProcess = Process.GetCurrentProcess();
+                var runningProcesses = Process.GetProcessesByName(currentProcess.ProcessName);
+
+                foreach (var process in runningProcesses)
+                {
+                    if (process.Id != currentProcess.Id)
+                    {
+                        IntPtr mainWindowHandle = process.MainWindowHandle;
+                        if (mainWindowHandle != IntPtr.Zero)
+                        {
+                            ShowWindow(mainWindowHandle, SW_RESTORE);
+                            SetForegroundWindow(mainWindowHandle);
+                        }
+                        break;
+                    }
+                }
+                Shutdown();
+                return;
+            }
+
             DispatcherUnhandledException += (sender, args) =>
             {
                 MessageBox.Show($"UI Crash Prevented:\n{args.Exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-                args.Handled = true; // Prevents app from closing!
+                args.Handled = true;
             };
 
-            // CATCH ALL BACKGROUND THREAD CRASHES (Like Favicon/Title updates)
             AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
             {
                 if (args.ExceptionObject is Exception ex)
