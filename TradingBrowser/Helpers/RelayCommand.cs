e4f647c7aa26d4b558993abe449b168a3860a6bc
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-10 10:38:57 +0530

    Update RelayCommand.cs

--- TradingBrowser/Helpers/RelayCommand.cs
+++ TradingBrowser/Helpers/RelayCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Input;
 
 namespace TradingBrowser
@@ -6,10 +7,20 @@ namespace TradingBrowser
     {
         private readonly Action<object?> _execute;
         private readonly Predicate<object?>? _canExecute;
+
         public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
-        { _execute = execute; _canExecute = canExecute; }
+        { 
+            _execute = execute; 
+            _canExecute = canExecute; 
+        }
+
         public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
         public void Execute(object? parameter) => _execute(parameter);
-        public event EventHandler? CanExecuteChanged { add { CommandManager.RequerySuggested += value; } remove { CommandManager.RequerySuggested -= value; } }
+
+        public event EventHandler? CanExecuteChanged 
+        { 
+            add { CommandManager.RequerySuggested += value; } 
+            remove { CommandManager.RequerySuggested -= value; } 
+        }
     }
 }
